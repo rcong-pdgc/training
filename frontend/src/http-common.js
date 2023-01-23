49df2993e209e7991d08c178b04831417b4b7cca
rcong <richard.cong@pdgc.com>
2023-01-23 12:55:03 -0800

    Add slash

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://vxyqi9vmph.execute-api.us-west-2.amazonaws.com/default",
+  baseURL: "https://vxyqi9vmph.execute-api.us-west-2.amazonaws.com/default/",
   headers: {
     "Content-type": "application/json"
   }
