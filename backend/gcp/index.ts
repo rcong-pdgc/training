6c8b0058280da97498ff00f79297d3e7e9ba37c9
rcong-pdgc <122652815+rcong-pdgc@users.noreply.github.com>
2023-01-30 22:00:22 -0800

    Sample pulumi for gcp

--- backend/gcp/index.ts
+++ backend/gcp/index.ts
@@ -1,82 +1,31 @@
-import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
 
-const stack = pulumi.getStack();
-
-const lambdaRole = new aws.iam.Role("lambdaRole", {
-  assumeRolePolicy: {
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Action: "sts:AssumeRole",
-        Principal: {
-          Service: "lambda.amazonaws.com",
-        },
-        Effect: "Allow",
-        Sid: "",
-      },
-    ],
-  },
+/**
+ * Deploy a function using the default runtime.
+ */
+const greeting = new gcp.cloudfunctions.HttpCallbackFunction("greeting", (req: any, res: any) => {
+    res.send(`Greetings from ${req.body.name || "Google Cloud Functions"}!`);
 });
 
-const lambdaRoleAttachment = new aws.iam.RolePolicyAttachment("lambdaRoleAttachment", {
-  role: lambdaRole,
-  policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+const invoker = new gcp.cloudfunctions.FunctionIamMember("invoker", {
+    project: greeting.function.project,
+    region: greeting.function.region,
+    cloudFunction: greeting.function.name,
+    role: "roles/cloudfunctions.invoker",
+    member: "allUsers",
 });
 
-const lambda = new aws.lambda.Function("lambdaFunction", {
-  code: new pulumi.asset.AssetArchive({
-    ".": new pulumi.asset.FileArchive("./app"),
-  }),
-  runtime: "nodejs18.x",
-  role: lambdaRole.arn,
-  handler: "index.handler",
-  timeout: 30
+export const url = greeting.httpsTriggerUrl;
+
+/**
+ * Deploy a function using an explicitly set runtime.
+ */
+const runtime = "nodejs14"; // https://cloud.google.com/functions/docs/concepts/exec#runtimes
+const explicitRuntimeGreeting = new gcp.cloudfunctions.HttpCallbackFunction(`greeting-${runtime}`, {
+    runtime: runtime,
+    callback: (req: any, res: any) => {
+        res.send(`Greetings from ${req.body.name || "Google Cloud Functions"}!`);
+    },
 });
 
-const apigw = new aws.apigatewayv2.Api("httpApiGateway", {
-  protocolType: "HTTP",
-  target: lambda.invokeArn,
-  corsConfiguration: {
-    allowHeaders: ["origin", "content-type", "x-auth-token"],
-    allowMethods: ["*"],
-    allowOrigins: ["*"]
-  }
-});
-
-const lambdaPermission = new aws.lambda.Permission("lambdaPermission", {
-  action: "lambda:InvokeFunction",
-  principal: "apigateway.amazonaws.com",
-  function: lambda,
-  sourceArn: pulumi.interpolate`${apigw.executionArn}/*/*`,
-}, {dependsOn: [apigw, lambda]});
-
-const integration = new aws.apigatewayv2.Integration("lambdaIntegration", {
-  apiId: apigw.id,
-  integrationType: "AWS_PROXY",
-  integrationUri: lambda.arn,
-  integrationMethod: "POST",
-  payloadFormatVersion: "2.0",
-  passthroughBehavior: "WHEN_NO_MATCH",
-});
-
-// const route = new aws.apigatewayv2.Route("apiRoute", {
-//   apiId: apigw.id,
-//   routeKey: "$default",
-//   target: pulumi.interpolate`integrations/${integration.id}`,
-// });
-
-// const stage = new aws.apigatewayv2.Stage("apiStage", {
-//   apiId: apigw.id,
-//   name: stack,
-//   routeSettings: [
-//     {
-//       routeKey: route.routeKey,
-//       throttlingBurstLimit: 5000,
-//       throttlingRateLimit: 10000,
-//     },
-//   ],
-//   autoDeploy: true,
-// }, {dependsOn: [route]});
-
-export const endpoint = pulumi.interpolate`${apigw.apiEndpoint}`;
+export const nodejs14Url = explicitRuntimeGreeting.httpsTriggerUrl;
